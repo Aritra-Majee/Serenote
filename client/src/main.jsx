d7993c9b44c9088f220f0e26a092cd7bd9e57a34
Aritra-Majee <aritramajee19@gmail.com>
2025-09-17 16:42:49 +0000

    fixed a syntax error in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from './contexts/authContext.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 import 'bootstrap-icons/font/bootstrap-icons.css';
